2c1e206e268828cd4046182e2088cb2971167bef
Mathieu Enault <mathieuenault@MacBook-Pro-de-Mathieu.local>
2022-02-14 17:31:53 +0100

    all countries display correctly

---
+++ src/components/card.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="card-container">
+      <img 
+        class="card__img"
+        style="height:80px; width:auto"
+        alt="product-image" 
+        :src="img"
+      >
+      <p>{{country}}</p>
+      <p>{{population}}</p>
+      <p>{{region}}</p>
+<p v-for="city in capital" :key="city">
+{{city}}
+ </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    img: String,
+    population:Number,
+    country:String,
+    region:String,
+    capital : Array
+  }
+}
+</script>
+=
+
+<style scoped>
+card-container {
+ width:300px;
+ height:500px;
+ background:yellow
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/pages/home.vue
@@ -0,0 +1,60 @@
+
+
+<script>
+import Card from "../components/card"
+export default {
+  name: 'Home',
+components:{
+  Card
+},
+  data(){
+    return{
+allCoutries: []
+  }
+
+},
+  beforeMount(){
+    this.getCountries();
+  },
+
+methods: {
+   async getCountries(){
+      const res = await fetch('https://restcountries.com/v3.1/all');
+      const data = await res.json();
+      console.log(data)
+      this.allCoutries = data;
+    }
+}
+}
+</script>
+
+<template>
+<h1>HomePage</h1>
+
+
+<div class="home-cards-container">
+<Card 
+v-for="(country, index ) in allCoutries" 
+:index="index"
+:img="country.flags.png"
+:key="country.name.common"
+:country="country.name.common"
+:population="country.population"
+:region="country.region"
+:capital="country.capital || undefined"
+>
+</Card>
+</div>
+
+
+</template>
+<style>
+.home-cards-container{
+  display:flex;
+  justify-content:space-evenly;
+  flex-wrap:wrap;
+
+}
+</style>
+
+

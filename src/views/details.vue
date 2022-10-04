6a9fa908b6c952aaffd929f941276c36605a8772
MathieuEnault <enault.mathieu2@gmail.com>
2022-10-04 21:42:50 +0200

    adding refresh card data on border country link click and improve responsive on details page

--- src/views/details.vue
+++ src/views/details.vue
@@ -19,7 +19,7 @@ export default {
       this.$router.push("/");
     },
     searchCountryWithCode(item) {
-      return this.$router.push(`/code/${item}`);
+      return this.$router.push({ name: "Details", params: { name: item } });
     },
     async getCountryDetails(name) {
       const code = name;
@@ -75,111 +75,113 @@ export default {
         .join(", ");
     },
   },
+  // evey time url params change we refresh card data
+  watch: {
+    "$route.params.name": {
+      handler: function (name) {
+        this.getCountryDetails(name);
+      },
+    },
+  },
 };
 </script>
 
 <template>
-  <div class="card-details-page-container" v-if="countryDetails">
-    <div class="go-back-container">
-      <button class="go-back-button" @click.prevent="goBack()">
+  <div class="details" v-if="countryDetails">
+    <div class="details__go-back-container">
+      <button class="details__go-back-button" @click.prevent="goBack()">
         <font-awesome-icon icon="arrow-left" color="gray" />
-        <span class="go-back-button-text">Back</span>
+        <span class="details__go-back-button-text">Back</span>
       </button>
     </div>
-    <section class="card-details-container">
-      <div class="card-details-items">
-        <div class="card-details-items-img">
+    <section class="details__card-container">
+      <div class="details__card-items">
+        <div class="details__card-items-img">
           <img
             :alt="`Flag of ${countryDetails.name.common}`"
             :src="countryDetails.flags.png"
           />
         </div>
 
-        <article class="card-details-content">
-          <div class="details-content-rows">
-            <span class="details-content-items-left-bloc">
-              <h2 class="card-details-name">
+        <article class="details__card-content">
+          <div class="details__content-rows">
+            <span>
+              <h2 class="details__content-title">
                 {{ countryDetails.name.common }}
               </h2>
             </span>
           </div>
 
-          <div class="details-content-rows">
-            <span class="details-content-items-left-bloc">
+          <div class="details__content-rows">
+            <span>
               <h3>
-                <span>Native name:</span>
-                <span class="items-value">
+                <span class="details__items-key">Native name:</span>
+                <span>
                   {{ valueOrNA(nativeNames) }}
                 </span>
               </h3>
             </span>
 
             <h3>
-              <span>Capital:</span>
-              <span class="items-value">
+              <span class="details__items-key">Capital:</span>
+              <span>
                 {{ valueOrNA(countryDetails.capital) }}
               </span>
             </h3>
           </div>
 
-          <div class="details-content-rows">
-            <span class="details-content-items-left-bloc">
+          <div class="details__content-rows">
+            <span>
               <h3>
-                <span>Population:</span>
-                <span class="items-value">
+                <span class="details__items-key">Population:</span>
+                <span>
                   {{ numberWithCommas }}
                 </span>
               </h3>
             </span>
 
             <h3>
-              <span>Currencies:</span>
-              <span class="items-value">
+              <span class="details__items-key">Currencies:</span>
+              <span>
                 {{ valueOrNA(currencies) }}
               </span>
             </h3>
           </div>
-          <div class="details-content-rows">
-            <span class="details-content-items-left-bloc">
+          <div class="details__content-rows">
+            <span>
               <h3>
-                <span>Région:</span>
-                <span class="items-value">
-                  {{ valueOrNA(countryDetails.region) }}</span
-                >
+                <span class="details__items-key">Région:</span>
+                <span> {{ valueOrNA(countryDetails.region) }}</span>
               </h3>
             </span>
             <h3>
-              <span>Languages:</span>
+              <span class="details__items-key">Languages:</span>
 
-              <span class="items-value">
+              <span>
                 {{ valueOrNA(languages) }}
               </span>
             </h3>
           </div>
-          <div class="details-content-rows">
-            <span class="details-content-items-left-bloc">
+          <div class="details__content-rows">
+            <span>
               <h3>
-                <span>Sub regions:</span>
-                <span class="items-value">
-                  {{ valueOrNA(countryDetails.subregion) }}</span
-                >
+                <span class="details__items-key">Sub regions:</span>
+                <span> {{ valueOrNA(countryDetails.subregion) }}</span>
               </h3>
             </span>
             <h3>
-              <span>Top Level Domain:</span>
-              <span class="items-value">
-                {{ valueOrNA(countryDetails.tld) }}</span
-              >
+              <span class="details__items-key">Top Level Domain:</span>
+              <span> {{ valueOrNA(countryDetails.tld) }}</span>
             </h3>
           </div>
 
-          <div class="borders-countries-container">
-            <h3 class="border-countries-title">Border Countries:</h3>
-            <div class="borders" v-if="countryDetails.borders">
+          <div class="details__borders-container">
+            <h3 class="details__border-countries">Border Countries:</h3>
+            <div class="details__borders" v-if="countryDetails.borders">
               <div v-for="(item, index) in countryDetails.borders" :key="index">
                 <ul>
                   <li
-                    class="borders-items"
+                    class="details__border-items"
                     @click="searchCountryWithCode(item)"
                   >
                     {{ item }}
@@ -194,18 +196,106 @@ export default {
   </div>
 </template>
 <style lang="scss">
-.card-details-page-container {
+.details {
   margin: auto;
   height: calc(100vh - 64px);
   padding: 0 80px;
-  .go-back-container {
+  &__go-back-container {
     display: flex;
     flex-direction: column;
     justify-content: center;
-    height: 180px;
+    height: 80px;
     width: 100%;
-
-    .go-back-button {
+  }
+  &__go-back-button {
+    width: 80px;
+    height: 30px;
+    border-radius: 4px;
+    box-shadow: rgb(60 64 67 / 15%) 0px 2px 6px 2px;
+    border: none;
+    font-weight: 600;
+    cursor: pointer;
+  }
+  &__go-back-button-text {
+    padding-left: 10px;
+    font-weight: 600;
+    font-size: 16px;
+  }
+  &__card-container {
+    width: 100%;
+    height: 400px;
+  }
+  &__card-items {
+    display: block;
+  }
+  &__card-items-img {
+    height: 200px;
+    img {
+      margin: 0;
+      height: 100%;
+      width: 100%;
+    }
+  }
+  &__card-content {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    overflow-x: hidden;
+  }
+  &__content-rows {
+    display: flex;
+    margin-top: 20px;
+    :first-child {
+      margin-right: 10px;
+    }
+  }
+  &__content-title {
+    font-size: 32px;
+  }
+  &__items-key {
+    font-size: 1rem;
+    text-decoration: underline;
+  }
+  &__borders-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 20px;
+    .borders ul {
+      margin: 6px;
+      padding: 3px 10px;
+      border-radius: 3px;
+      box-shadow: rgb(60 64 67 / 15%) 0px 2px 6px 2px;
+    }
+  }
+  &__border-countries {
+    font-size: 1rem;
+    text-decoration: underline;
+    text-align: left;
+    min-width: 128px;
+  }
+  &__borders {
+    display: flex;
+    overflow-x: auto;
+    width: auto;
+    justify-content: space-between;
+  }
+  &__border-items {
+    padding: 8px;
+    margin: 0 3px;
+    color: var(--switch-color);
+    background-color: var(--text-primary-color);
+    &:hover {
+      cursor: pointer;
+    }
+  }
+}
+@media (min-width: 670px) {
+  .details {
+    &__go-back-container {
+      height: 180px;
+    }
+    &__go-back-button {
       width: 130px;
       height: 40px;
       border-radius: 4px;
@@ -213,147 +303,44 @@ export default {
       border: none;
       font-weight: 600;
       cursor: pointer;
-      .go-back-button-text {
-        padding-left: 10px;
-        font-weight: 600;
-        font-size: 16px;
-      }
     }
-  }
-
-  .card-details-container {
-    width: 100%;
-    height: 400px;
-    .card-details-items {
+    &__card-items {
       display: flex;
-
+      height: 100%;
+    }
+    &__card-items-img {
       img {
         height: 400px;
-        margin-right: 100px;
       }
-      .card-details-content {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-evenly;
-        overflow-x: hidden;
-
-        .details-content-rows {
-          display: flex;
-          .items-value {
-            margin-left: 10px;
-          }
-          .details-content-items {
-            width: 50%;
-            &-left-bloc {
-              width: 50%;
-              text-align: left;
-
-              h2.card-details-name {
-                font-size: 32px;
-              }
-            }
-          }
-        }
-
-        .borders-countries-container {
-          display: flex;
-          align-items: center;
-
-          .border-countries-title {
-            width: 130px;
-          }
-          .borders {
-            display: flex;
-            overflow-x: auto;
-          }
-
-          .borders ul {
-            margin: 10px;
-            padding: 3px 20px;
-            border-radius: 3px;
-            box-shadow: rgb(60 64 67 / 15%) 0px 2px 6px 2px;
-          }
-          .borders-items {
-            &:hover {
-              cursor: pointer;
-            }
-          }
-        }
+    }
+    &__card-content {
+      margin-left: 20px;
+      width: 100%;
+      max-width: 660px;
+    }
+    &__content-rows {
+      margin-top: 0px;
+    }
+    &__borders-container {
+      margin-top: 0px;
+      .borders ul {
+        margin: 6px;
+        padding: 3px 10px;
       }
     }
   }
 }
 
-@media (max-width: 1500px) {
-  .card-details-page-container {
+@media (max-width: 1200px) {
+  .details {
     padding: 0 20px;
-    .card-details-container {
-      width: 100%;
-      height: 300px;
-      .card-details-items {
-        img {
-          height: 300px;
-          margin-right: 100px;
-        }
-        .details-content-rows {
-          display: block;
-        }
-        .borders-countries-container {
-          display: flex;
-          .borders ul {
-            padding: 3px 10px;
-            margin: 5px;
-          }
-        }
-      }
-    }
-  }
-}
-
-@media (max-width: 1200px) and (min-width: 515px) {
-  .card-details-page-container {
-    .go-back-container {
-      height: 80px;
-      width: 100%;
-    }
-
-    .card-details-container {
-      margin: 0 15%;
-      width: 70%;
-      height: 800px;
+    &__content-rows {
       display: block;
-      .card-details-items {
-        display: block;
-        .card-details-items-img {
-          text-align: center;
-          img {
-            height: auto;
-            width: 100%;
-            margin: 0;
-          }
-        }
-      }
+      text-align: left;
+      font-size: 0.8rem;
     }
-  }
-}
-
-@media (max-width: 514px) and (min-width: 320px) {
-  .card-details-page-container {
-    .go-back-container {
-      height: 80px;
-    }
-    .card-details-container {
-      .card-details-items {
-        display: block;
-        .card-details-items-img {
-          height: 200px;
-          img {
-            margin: 0;
-            height: 100%;
-            width: 100%;
-          }
-        }
-      }
+    &__borders-container {
+      display: flex;
     }
   }
 }
